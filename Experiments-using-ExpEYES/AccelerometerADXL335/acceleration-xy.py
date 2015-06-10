431638a5ffbd7592b0b22ccebe5dd25119fc76ef
Praveen (Gnovi) <wavicles@users.noreply.github.com>
2015-06-10 07:30:55 +0530

    Update acceleration-xy.py

--- Experiments-using-ExpEYES/AccelerometerADXL335/acceleration-xy.py
+++ Experiments-using-ExpEYES/AccelerometerADXL335/acceleration-xy.py
@@ -1,4 +1,80 @@
-	self.tv[0].append(elapsed)
+	'''
+expEYES program
+Developed as a part of GSoC- project  
+License : GNU GPL version 3
+
+Program to plot acceleration data using ADXL335 sensor in real-time
+
+'''
+import gettext
+gettext.bindtextdomain("expeyes")
+gettext.textdomain('expeyes')
+_ = gettext.gettext
+
+
+import time, math, sys
+if sys.version_info.major==3:
+        from tkinter import *
+else:
+        from Tkinter import *
+
+sys.path=[".."] + sys.path
+
+import expeyes.eyesj as eyes
+import expeyes.eyeplot as eyeplot
+import expeyes.eyemath as eyemath
+
+
+
+WIDTH  = 600   # width of drawing canvas
+HEIGHT = 400   # height    
+
+class Accl:
+	tv = [ [], [], [], [] ]		# Three Lists for Readings time, v1 and v2
+	TIMER = 5			# Time interval between reads
+	MINY = -5			# Voltage range
+	MAXY = 5
+	running = False
+	MAXTIME = 10
+	
+
+	def xmgrace(self):
+		if self.running == True:
+			return
+		p.grace([self.tv])
+
+	def start(self):
+		
+		print p.set_voltage(5.0)
+		self.running = True
+		self.index = 0
+		self.tv = [ [], [], [], [] ]
+		try:
+			self.MAXTIME = int(DURATION.get())
+			g.setWorld(0, self.MINY, self.MAXTIME, self.MAXY,_('Time'),_('m/s^2'))
+			Dur.config(state=DISABLED)
+			self.msg(_('Starting the Measurements'))
+			root.after(self.TIMER, self.update)
+		except:
+			self.msg(_('Failed to Start'))
+
+	def stop(self):
+		self.running = False
+		Dur.config(state=NORMAL)
+		self.msg(_('User Stopped the measurements'))
+
+	def update(self):
+		if self.running == False:
+			return
+		t,v = p.get_voltage_time(1)  # Read A1
+		v2 = p.get_voltage(2)
+		v3 = p.get_voltage(3)
+		if len(self.tv[0]) == 0:
+			self.start_time = t
+			elapsed = 0
+		else:
+			elapsed = t - self.start_time
+		self.tv[0].append(elapsed)
 		self.tv[1].append(v)
 		self.tv[2].append(v2)
 		self.tv[3].append(v3)
@@ -78,7 +154,3 @@ mf.pack(side=TOP)
 msgwin = Label(mf,text=_('Message'), fg = 'blue')
 msgwin.pack(side=LEFT, anchor = S, fill=BOTH, expand=1)
 
-
-eyeplot.pop_image('pics/image-name.png', _('acceration using accelerometer'))  # save the image in the same directory as of the program
-root.title(_('Acceleration using ADXL335'))
-root.mainloop()
