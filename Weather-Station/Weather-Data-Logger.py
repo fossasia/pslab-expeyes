53b403e77bc9a4971744dd718812cf6a69ea1dca
Praveen (Gnovi) <wavicles@users.noreply.github.com>
2015-07-01 22:31:40 +0530

    Update Weather-Data-Logger.py
    Successful data-logging trial with two channels

--- Weather-Station/Weather-Data-Logger.py
+++ Weather-Station/Weather-Data-Logger.py
@@ -2,7 +2,6 @@
 ExpEYES Program written as a part og GSoC Project with FOSSASIA
 This programme is for logging weather data like temperature,barometric pressure, humidity and wind speed.
 '''
-
 import gettext
 gettext.bindtextdomain("expeyes")
 gettext.textdomain('expeyes')
@@ -21,23 +20,19 @@ import expeyes.eyesj as eyes
 import expeyes.eyeplot as eyeplot
 import expeyes.eyemath as eyemath
 
+WIDTH  = 1000   # width of drawing canvas
+HEIGHT = 600   # height    
 
 
-WIDTH  = 800   # width of drawing canvas
-HEIGHT = 600   # height of the drawing canvas   
-
-class Weather:
-	tv = [ [], [], [], [] ]		# Three Lists for Readings time, v1 , v2 and v3
-	TIMER = 5			# Time interval between reads
-	MINY = -5			# Voltage range
-	MAXY = 5
+class WS:
+	tv = [ [], [], [] ]		# Three Lists for Readings time, v1 and v2
+	TIMER = 500			# Time interval between reads
+	MINY = 0			# Voltage range
+	MAXY = 5.0
 	running = False
 	MAXTIME = 10
 
-	def xmgrace(self):
-		if self.running == True:
-			return
-		p.grace([self.tv])
+	
 
 	def start(self):
 		self.running = True
@@ -45,23 +40,28 @@ class Weather:
 		self.tv = [ [], [], [] ]
 		try:
 			self.MAXTIME = int(DURATION.get())
-			g.setWorld(0, self.MINY, self.MAXTIME, self.MAXY,_('Time'),_('Volt'))
+			g.setWorld(0, self.MINY, self.MAXTIME, self.MAXY,_('Time'),_('Data'))
+			self.TIMER = int(TGAP.get())
+			Total.config(state=DISABLED)
 			Dur.config(state=DISABLED)
 			self.msg(_('Starting the Measurements'))
 			root.after(self.TIMER, self.update)
 		except:
 			self.msg(_('Failed to Start'))
+			pass
 
 	def stop(self):
 		self.running = False
+		Total.config(state=NORMAL)
 		Dur.config(state=NORMAL)
 		self.msg(_('User Stopped the measurements'))
+
 
 	def update(self):
 		if self.running == False:
 			return
 		t,v = p.get_voltage_time(1)  # Read A1
-		v2 = p.get_voltage(2)
+		v1 = p.get_voltage(2)
 		if len(self.tv[0]) == 0:
 			self.start_time = t
 			elapsed = 0
@@ -69,23 +69,25 @@ class Weather:
 			elapsed = t - self.start_time
 		self.tv[0].append(elapsed)
 		self.tv[1].append(v)
-		self.tv[2].append(v2)
+		self.tv[2].append(v1)
 		if len(self.tv[0]) >= 2:
 			g.delete_lines()
 			g.line(self.tv[0], self.tv[1])
 			g.line(self.tv[0], self.tv[2],1)
 		if elapsed > self.MAXTIME:
 			self.running = False
+			Total.config(state=NORMAL)
 			Dur.config(state=NORMAL)
 			self.msg(_('Completed the Measurements'))
 			return 
 		root.after(self.TIMER, self.update)
 
+
 	def save(self):
 		try:
 			fn = filename.get()
 		except:
-			fn = 'coupledpend.dat'
+			fn = 'weather-station.dat'
 		p.save([self.tv],fn)
 		self.msg(_('Data saved to %s')%fn)
 
@@ -99,41 +101,54 @@ class Weather:
 	def msg(self,s, col = 'blue'):
 		msgwin.config(text=s, fg=col)
 
+
+
 p = eyes.open()
 p.disable_actions()
 root = Tk()
-Canvas(root, width = WIDTH, height = 5).pack(side=TOP)  
-g = eyeplot.graph(root, width=WIDTH, height=HEIGHT, bip=False)	
-pen = Weather()
+Canvas(root, width = WIDTH, height = 5).pack(side=TOP)  # Some space at the top
+g = eyeplot.graph(root, width=WIDTH, height=HEIGHT, bip=False)	# make plot objects using draw.disp
+log = WS()
+
 
 cf = Frame(root, width = WIDTH, height = 10)
 cf.pack(side=TOP,  fill = BOTH, expand = 1)
 
+b3 = Label(cf, text = _('Read Every'))
+b3.pack(side = LEFT, anchor = SW)
+TGAP = StringVar()
+Dur =Entry(cf, width=5, bg = 'white', textvariable = TGAP)
+TGAP.set('1000')
+Dur.pack(side = LEFT, anchor = SW)
+b3 = Label(cf, text = _('mS,'))
+b3.pack(side = LEFT, anchor = SW)
 
-b3 = Label(cf, text = _('Digitize for'))
+
+b3 = Label(cf, text = _('for total'))
 b3.pack(side = LEFT, anchor = SW)
 DURATION = StringVar()
-Dur =Entry(cf, width=5, bg = 'white', textvariable = DURATION)
+Total =Entry(cf, width=5, bg = 'white', textvariable = DURATION)
 DURATION.set('100')
-Dur.pack(side = LEFT, anchor = SW)
-b3 = Label(cf, text = _('Seconds.'))
+Total.pack(side = LEFT, anchor = SW)
+b3 = Label(cf, text = _('Seconds'))
 b3.pack(side = LEFT, anchor = SW)
 
 cf = Frame(root, width = WIDTH, height = 10)
 cf.pack(side=TOP,  fill = BOTH, expand = 1)
-b1 = Button(cf, text = _('START'), command = pen.start)
+
+
+b1 = Button(cf, text = _('START'), command = log.start)
 b1.pack(side = LEFT, anchor = N)
-b1 = Button(cf, text = _('STOP'), command = pen.stop)
+b1 = Button(cf, text = _('STOP'), command = log.stop)
 b1.pack(side = LEFT, anchor = N)
-b4 = Button(cf, text = _('CLEAR'), command = pen.clear)
+b4 = Button(cf, text = _('CLEAR'), command = log.clear)
 b4.pack(side = LEFT, anchor = N)
-b1 = Button(cf, text = _('Xmgrace'), command = pen.xmgrace)
-b1.pack(side = LEFT, anchor = N)
-b3 = Button(cf, text = _('SAVE to'), command = pen.save)
+
+b3 = Button(cf, text = _('SAVE to'), command = log.save)
 b3.pack(side = LEFT, anchor = N)
 filename = StringVar()
 e1 =Entry(cf, width=15, bg = 'white', textvariable = filename)
-filename.set('weather.dat')
+filename.set('weather-station.dat')
 e1.pack(side = LEFT)
 b5 = Button(cf, text = _('QUIT'), command = sys.exit)
 b5.pack(side = RIGHT, anchor = N)
@@ -144,6 +159,6 @@ msgwin = Label(mf,text=_('Message'), fg = 'blue')
 msgwin.pack(side=LEFT, anchor = S, fill=BOTH, expand=1)
 
 
-eyeplot.pop_image('pics/image-name.png', _(''))  
+eyeplot.pop_image('pics/image-name.png', _('---'))  # save the image in the same directory as of the program
 root.title(_('Weather Station Data Logger'))
 root.mainloop()
