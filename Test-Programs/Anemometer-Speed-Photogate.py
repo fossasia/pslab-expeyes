063ebd00f193ed5f5ad3e193f4fc5bcf092416a7
Praveen (Gnovi) <wavicles@users.noreply.github.com>
2015-07-13 21:34:43 +0530

    Update Anemometer-Speed-Photogate.py

--- Test-Programs/Anemometer-Speed-Photogate.py
+++ Test-Programs/Anemometer-Speed-Photogate.py
@@ -7,5 +7,6 @@ Use function print p.get_frequency(3) # to get frequency of square wave at at pi
 import expeyes.eyemath as em
 p = expeyes.eyesj.open()
 p.set_state(10,1)   # sets OD1 at HIGH, Channel No for OD1 id 10
-print p.get_frequency(3)
+print p.get_frequency(3) # function to measure frequency 
+# magnetic sensor can also be used for this purpose
 
