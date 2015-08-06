8df1f48a0088574140590c66c17d54e57fb9271a
Praveen (Gnovi) <wavicles@users.noreply.github.com>
2015-08-07 00:34:34 +0530

    Create tweet.py

---
+++ Auto-Tweet-Data/tweet.py
@@ -0,0 +1,22 @@
+import tweepy
+from datetime import datetime
+
+API_KEY = 'YOUR API KEY'
+API_SECRET = 'YOUR API SECRET'
+ACCESS_TOKEN = 'YOUR ACCESS TOKEN'
+ACCESS_TOKEN_SECRET = 'YOUR ACCESS TOKEN SECRET'
+
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+tempfile = open("address of the weather data file")
+thetext = tempfile.read()
+tempfile.close()
+tempdata = thetext.split("\n")[1].split(" ")[9]
+temperature = float(tempdata[2:])
+temperature = str(temperature / 1000)
+
+thetime = datetime.now().strftime('%-I:%M%P on %d-%m-%Y')
+
+api.update_status(temperature + " C at " + thetime)
