97a8848f8c10141e3aec1356c2a6cd5ab9c73357
Praveen (Gnovi) <wavicles@users.noreply.github.com>
2015-08-09 00:08:56 +0530

    Update Temp-LM35-GUI.py

--- Tempreature-Sensor/Temp-LM35-GUI.py
+++ Tempreature-Sensor/Temp-LM35-GUI.py
@@ -72,7 +72,7 @@ class LM35:
 	def update(self):
 		if self.running == False:
 			return
-		t,v = p.get_voltage_time(4)  # # Read IN2 as IN1 is useful for capacity measurements and is required for HS1101 sensor for humidity measurements
+		t,v = p.get_voltage_time(4)  # Read A1
 		if len(self.tv[0]) == 0:
 			self.start_time = t
 			elapsed = 0
@@ -109,3 +109,92 @@ class LM35:
 	def clear(self):
 		if self.running == True:
 			return
+		self.nt = [ [], [] ]
+		g.delete_lines()
+		self.msg(_('Cleared Data and Trace'))
+
+	def msg(self,s, col = 'blue'):
+		msgwin.config(text=s, fg=col)
+
+	def quit(self):
+		p.set_state(10,0)
+		sys.exit()
+
+p = eyes.open()
+p.disable_actions()
+
+root = Tk()
+Canvas(root, width = WIDTH, height = 5).pack(side=TOP)  
+
+g = eyeplot.graph(root, width=WIDTH, height=HEIGHT, bip=False)
+pt = LM35()
+
+cf = Frame(root, width = WIDTH, height = 10)
+cf.pack(side=TOP,  fill = BOTH, expand = 1)
+
+b3 = Label(cf, text = _('Read Every'))
+b3.pack(side = LEFT, anchor = SW)
+TGAP = StringVar()
+Dur =Entry(cf, width=5, bg = 'white', textvariable = TGAP)
+TGAP.set('500')
+Dur.pack(side = LEFT, anchor = SW)
+b3 = Label(cf, text = _('mS,'))
+b3.pack(side = LEFT, anchor = SW)
+b3 = Label(cf, text = _('for total'))
+b3.pack(side = LEFT, anchor = SW)
+DURATION = StringVar()
+Total =Entry(cf, width=5, bg = 'white', textvariable = DURATION)
+DURATION.set('100')
+Total.pack(side = LEFT, anchor = SW)
+b3 = Label(cf, text = _('Seconds.'))
+b3.pack(side = LEFT, anchor = SW)
+
+b3 = Label(cf, text = _('Range'))
+b3.pack(side = LEFT, anchor = SW)
+TMIN = StringVar()
+TMIN.set('0')
+Tmin =Entry(cf, width=5, bg = 'white', textvariable = TMIN)
+Tmin.pack(side = LEFT, anchor = SW)
+b3 = Label(cf, text = _('to,'))
+b3.pack(side = LEFT, anchor = SW)
+TMAX = StringVar()
+TMAX.set('100')
+Tmax =Entry(cf, width=5, bg = 'white', textvariable = TMAX)
+Tmax.pack(side = LEFT, anchor = SW)
+b3 = Label(cf, text = _('C. '))
+
+b3 = Button(cf, text = _('SAVE to'), command = pt.save)
+b3.pack(side = LEFT, anchor = SW)
+b3.pack(side = LEFT, anchor = SW)
+filename = StringVar()
+e1 =Entry(cf, width=15, bg = 'white', textvariable = filename)
+filename.set('temperature.dat')
+e1.pack(side = LEFT, anchor = SW)
+
+cf = Frame(root, width = WIDTH, height = 10)
+cf.pack(side=TOP,  fill = BOTH, expand = 1)
+
+b1 = Button(cf, text = _('Xmgrace'), command = pt.xmgrace)
+b1.pack(side = LEFT, anchor = SW)
+
+
+cf = Frame(root, width = WIDTH, height = 10)
+cf.pack(side=TOP,  fill = BOTH, expand = 1)
+e1.pack(side = LEFT)
+
+b5 = Button(cf, text = _('QUIT'), command = pt.quit)
+b5.pack(side = RIGHT, anchor = N)
+b4 = Button(cf, text = _('CLEAR'), command = pt.clear)
+b4.pack(side = RIGHT, anchor = N)
+b1 = Button(cf, text = _('STOP'), command = pt.stop)
+b1.pack(side = RIGHT, anchor = N)
+b1 = Button(cf, text = _('START'), command = pt.start)
+b1.pack(side = RIGHT, anchor = N)
+
+mf = Frame(root, width = WIDTH, height = 10)
+mf.pack(side=TOP)
+msgwin = Label(mf,text=_('Message'), fg = 'blue')
+msgwin.pack(side=LEFT, anchor = S, fill=BOTH, expand=1)
+
+root.title(_('Temperature Measuements using LM35'))
+root.mainloop()
