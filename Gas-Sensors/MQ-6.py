00d69e1c7c77397fb640fb6f34b9537a95d5e809
Praveen (Gnovi) <wavicles@users.noreply.github.com>
2015-08-16 01:14:25 +0530

    Update MQ-6.py

--- Gas-Sensors/MQ-6.py
+++ Gas-Sensors/MQ-6.py
@@ -1,4 +1,20 @@
+'''
+GUI for interfacing LPG Gas Sensor MQ-6 with ExpEYES
+The GUI program is same for all gas sensors except the calibration factor 
+to convert output voltage to gas concentartion in ppm.
 
+ExpEYES program developed as a part of GSoC-2015 project
+Project Tilte: Sensor Plug-ins, Add-on devices and GUI Improvements for ExpEYES
+
+Mentor Organization:FOSSASIA
+Mentors: Hong Phuc, Mario Behling, Rebentisch
+Author: Praveen Patil
+License : GNU GPL version 3
+
+
+'''
+
+#Connections vcc to OD1, GND to GND and AD to IN2
 
 import gettext
 gettext.bindtextdomain("expeyes")
