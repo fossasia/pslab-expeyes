76b5ea346f93732d0836ec5667a1a50232f16d23
Praveen (Gnovi) <wavicles@users.noreply.github.com>
2015-08-17 22:12:13 +0530

    Update PressureSensor.py

--- Pressure-Sensor/PressureSensor.py
+++ Pressure-Sensor/PressureSensor.py
@@ -1,7 +1,16 @@
 '''
 A simple code to test pressure sensor SPD015AAsil by SMARTEC
 
-Connections
+ExpEYES program developed as a part of GSoC-2015 project
+Project Tilte: Sensor Plug-ins, Add-on devices and GUI Improvements for ExpEYES
+Mentor Organization:FOSSASIA
+Mentors: Hong Phuc, Mario Behling, Rebentisch
+Author: Praveen Patil
+License : GNU GPL version 3
+
+
+
+**Connections**
 Pin 1 and Pin 5 not connected
 Pin 2 VCC  Connect to PVS of ExpEYES at +5V , Pin 3 Output  to IN1 / A1
 Pin 4  GND  Between pin 2 and 4 connect a 100nF capacitor
