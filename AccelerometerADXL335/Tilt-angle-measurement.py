c89a319364d9cfaded842b656f60f78bae430847
Praveen (Gnovi) <wavicles@users.noreply.github.com>
2015-08-18 15:39:35 +0530

    Create Tilt-angle-measurement.py

---
+++ AccelerometerADXL335/Tilt-angle-measurement.py
@@ -0,0 +1,7 @@
+'''
+Accelerometer ADXL 335 can be used for measuring Tilt angle
+ADXL335 acceleration measurement range is +/- 3 g. Supply voltage is 1.8 –  3.6 V, however all specifications at the datasheet is given at 3.0 V. This accelerometer has  3 outputs for X,Y,Z axis which voltage is proportional to acceleration on specific axis.
+
+At midpoint when acceleration is 0 g output is typically 1/2 of supply voltage. If a supply voltage is 3V, then output is 1.5 V. Output sensitivity typically is 300 mV/g.
+
+'''
