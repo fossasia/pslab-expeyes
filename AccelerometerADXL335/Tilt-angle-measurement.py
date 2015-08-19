fc30a4a7e7a31b7a7e7d5d672f62afcdc4787dc6
Praveen (Gnovi) <wavicles@users.noreply.github.com>
2015-08-19 22:23:10 +0530

    atan2 function description & minor update

--- AccelerometerADXL335/Tilt-angle-measurement.py
+++ AccelerometerADXL335/Tilt-angle-measurement.py
@@ -17,6 +17,11 @@ For 	0g  	v = 1.61 volt
 	+1g 	v = 1.91 volt
 Sensitivity 	0.3v/g
  
+For tilt angle calculation:
+atan2(y, x)-------- Returns atan(y / x), in radians. The result is between -pi and pi. The vector in the plane from the origin to point (x, y) makes this angle with the positive X axis. The point of atan2() is that the signs of both inputs are known to it, so it can compute the correct quadrant for the angle. For example, atan(1) and atan2(1, 1) are both pi/4, but atan2(-1, -1) is -3*pi/4.
+
+
+
 '''
 import gettext					#Internationalization
 gettext.bindtextdomain("expeyes")
@@ -53,10 +58,10 @@ Xaccl = (v-1.6) / 0.3
 Yaccl = (v2-1.6) / 0.3
 Zaccl = (v3-1.6) / 0.3
 
-angle_x =atan2(-Yaccl,-Zaccl)*57.2957795+180;	# math.atan2(y, x) Return atan(y / x), in radians. The result is between -pi and pi. The vector in the plane from the origin to point (x, y) makes this angle with the positive X axis. The point of atan2() is that the signs of both inputs are known to it, so it can compute the correct quadrant for the angle. For example, atan(1) and atan2(1, 1) are both pi/4, but atan2(-1, -1) is -3*pi/4.
-
+angle_x =atan2(-Yaccl,-Zaccl)*57.2957795+180;	# The rotation(for x axis) is calculated using atan2 function. It calculates angle from length of y, z vectors. *57.2957795 – is conversation of radian to degree. +180 is for offset.
 
 angle_y =atan2(-Zaccl,-Xaccl)*57.2957795+180;
+
 angle_z =atan2(-Xaccl,-Yaccl)*57.2957795+180;
 
 print "Tilt angle X-axis = ", angle_x, "degree"
