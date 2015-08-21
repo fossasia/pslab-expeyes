29642a39f7f1976116d69dfc6ca6ff8cd569aada
Praveen (Gnovi) <wavicles@users.noreply.github.com>
2015-08-21 20:24:44 +0530

    Update charging-discharging.py

--- Battery-Characteristics/charging-discharging.py
+++ Battery-Characteristics/charging-discharging.py
@@ -1,5 +1,13 @@
 '''
+GUI Program to plot charging discharging curve for re-chargeable batteries in real-time
 
+
+ExpEYES program developed as a part of GSoC-2015 project
+Project Tilte: Sensor Plug-ins, Add-on devices and GUI Improvements for ExpEYES
+Mentor Organization:FOSSASIA
+Mentors: Hong Phuc, Mario Behling, Rebentisch
+Author: Praveen Patil
+License : GNU GPL version 3
 '''
 import gettext
 gettext.bindtextdomain("expeyes")
