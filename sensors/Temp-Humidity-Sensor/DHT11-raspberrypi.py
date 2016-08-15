c04384f7f13ca77fa62a88de3a86495335417141
georgesk <georgesk@debian.org>
2016-08-15 14:46:15 +0200

    tried to make some sense by indenting the code; the result is not yest debugged

--- sensors/Temp-Humidity-Sensor/DHT11-raspberrypi.py
+++ sensors/Temp-Humidity-Sensor/DHT11-raspberrypi.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import RPi.GPIO as GPIO
 import time
  
@@ -27,61 +28,61 @@ TemperatureBit = ""
 crc = ""
  
 try:
-while data[count] == 1:
-tmp = 1
-count = count + 1
+    while data[count] == 1:
+        tmp = 1
+        count = count + 1
  
-for i in range(0, 32):
-bit_count = 0
- 
-while data[count] == 0:
-tmp = 1
-count = count + 1
- 
-while data[count] == 1:
-bit_count = bit_count + 1
-count = count + 1
- 
-if bit_count > 3:
-if i>=0 and i<8:
-HumidityBit = HumidityBit + "1"
-if i>=16 and i<24:
-TemperatureBit = TemperatureBit + "1"
-else:
-if i>=0 and i<8:
-HumidityBit = HumidityBit + "0"
-if i>=16 and i<24:
-TemperatureBit = TemperatureBit + "0"
+    for i in range(0, 32):
+        bit_count = 0
+         
+    while data[count] == 0:
+        tmp = 1
+        count = count + 1
+         
+    while data[count] == 1:
+        bit_count = bit_count + 1
+        count = count + 1
+         
+    if bit_count > 3:
+        if i>=0 and i<8:
+            HumidityBit = HumidityBit + "1"
+        if i>=16 and i<24:
+            TemperatureBit = TemperatureBit + "1"
+        else:
+            if i>=0 and i<8:
+                HumidityBit = HumidityBit + "0"
+            if i>=16 and i<24:
+                TemperatureBit = TemperatureBit + "0"
  
 except:
-print "ERR_RANGE"
-exit(0)
+    print ("ERR_RANGE")
+    exit(0)
  
 try:
-for i in range(0, 8):
-bit_count = 0
+    for i in range(0, 8):
+        bit_count = 0
  
-while data[count] == 0:
-tmp = 1
-count = count + 1
+    while data[count] == 0:
+        tmp = 1
+        count = count + 1
  
-while data[count] == 1:
-bit_count = bit_count + 1
-count = count + 1
+    while data[count] == 1:
+        bit_count = bit_count + 1
+        count = count + 1
  
-if bit_count > 3:
-crc = crc + "1"
-else:
-crc = crc + "0"
+    if bit_count > 3:
+        crc = crc + "1"
+    else:
+        crc = crc + "0"
 except:
-print "ERR_RANGE"
-exit(0)
+    print ("ERR_RANGE")
+    exit(0)
 
 Humidity = bin2dec(HumidityBit)
 Temperature = bin2dec(TemperatureBit)
  
 if int(Humidity) + int(Temperature) - int(bin2dec(crc)) == 0:
-print "Humidity:"+ Humidity +"%"
-print "Temperature:"+ Temperature +"C"
+    print ("Humidity:"+ Humidity +"%")
+    print ("Temperature:"+ Temperature +"C")
 else:
-print "ERR_CRC"
+    print ("ERR_CRC")
