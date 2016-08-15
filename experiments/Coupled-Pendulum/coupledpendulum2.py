0c89576f79ee84a13e4490f6a1905492fd8a4c7b
georgesk <georgesk@debian.org>
2016-08-15 12:03:15 +0200

    reindented blocks, made print function compatible with python3

--- experiments/Coupled-Pendulum/coupledpendulum2.py
+++ experiments/Coupled-Pendulum/coupledpendulum2.py
@@ -10,6 +10,7 @@ This experiment can be done in three different ways
 2. Both the pendula oscillating out-of-phase
 3. One pendulum at rest while other is set in to oscillations.
 '''
+from __future__ import print_function
 import gettext
 gettext.bindtextdomain("expeyes")
 gettext.textdomain('expeyes')
@@ -26,43 +27,43 @@ f = open('pend_wave.dat','w')
 start = p.get_voltage_time(1)[0]
 start2 = p.get_voltage_time(2)[0]
 while 1:
-res = p.get_voltage_time(1)
-tm = res[0] - start # elapsed time
-ta.append(tm)
-va.append(res[1])
-res2 = p.get_voltage_time(2)
-tm2 = res2[0] - start # elapsed time
-tb.append(tm2)
-vb.append(res2[1])
-ss = '%6.3f %6.3f %6.3f %6.3f'%(tm,res[1],tm2,res2[1])
-print ss
-f.write(ss+'\n')
-if tm > DURATION:
-break
-subplot(3,1,1)
-plot(ta,va,'r') #position Plot
-title('Pendulum - 1')
-xlabel('Time')
-ylabel('Displacement')
-subplot(3,1,2)
-plot(tb,vb, 'b')
-title('Pendulum - 2')
-xlabel('Time')
-ylabel('Displacement')
-subplot(3,1,3)
-plot(ta,va,'r', tb,vb, 'b')
-title('Coupled Pendula')
-xlabel('Time')
-ylabel('Displacement')
-plt.tight_layout() #this function from matplotlib provides spacing between subgraphs
-show()
-'''
-# This code can be used to have plots in separate figures
-figure(1)
-plot(ta,va)
-figure(2)
-plot(tb,vb)
-figure(3)
-plot(ta,va, tb,vb)
-show()
-'''
+    res = p.get_voltage_time(1)
+    tm = res[0] - start # elapsed time
+    ta.append(tm)
+    va.append(res[1])
+    res2 = p.get_voltage_time(2)
+    tm2 = res2[0] - start # elapsed time
+    tb.append(tm2)
+    vb.append(res2[1])
+    ss = '%6.3f %6.3f %6.3f %6.3f'%(tm,res[1],tm2,res2[1])
+    print (ss)
+    f.write(ss+'\n')
+    if tm > DURATION:
+        break
+    subplot(3,1,1)
+    plot(ta,va,'r') #position Plot
+    title('Pendulum - 1')
+    xlabel('Time')
+    ylabel('Displacement')
+    subplot(3,1,2)
+    plot(tb,vb, 'b')
+    title('Pendulum - 2')
+    xlabel('Time')
+    ylabel('Displacement')
+    subplot(3,1,3)
+    plot(ta,va,'r', tb,vb, 'b')
+    title('Coupled Pendula')
+    xlabel('Time')
+    ylabel('Displacement')
+    plt.tight_layout() #this function from matplotlib provides spacing between subgraphs
+    show()
+    '''
+    # This code can be used to have plots in separate figures
+    figure(1)
+    plot(ta,va)
+    figure(2)
+    plot(tb,vb)
+    figure(3)
+    plot(ta,va, tb,vb)
+    show()
+    '''
